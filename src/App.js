d9bf7bc3a169e8ebc96311b57a742d12ddd307ce
Nishant Kumar <nishantdinayak11@gmail.com>
2021-11-03 03:06:30 +0530

    hello :! duck duck go

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://duckduckgo.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Duck Duck Go
         </a>
       </header>
     </div>
